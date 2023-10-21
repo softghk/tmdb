12674501622c67670d124cfee75422559b0a926d
Hossein Kavand <hosseinkavand9@gmail.com>
2023-10-21 09:56:32 +0330

    handle fix and remove the useless files

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,4 +1,19 @@
-export * from './movie'
+export interface MovieItemType {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: false
+  vote_average: number
+  vote_count: number
+}
 
 export interface ResponsePagination<T> {
   page: number
--- src/services/urls.ts
+++
@@ -1,1 +0,0 @@
-export const MOVIE_LIST = `3/discover/movie`
